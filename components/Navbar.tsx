a9c920a276ed0ef25b06a3983e4ce49fd37797af
RT <100726118+team7202@users.noreply.github.com>
2022-08-31 18:12:08 -0400

    Changed a tags to Link tags

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,14 @@ import {
     QuestionMarkCircleIcon,
     VideoCameraIcon
 } from "@heroicons/react/solid";
+import Link from "next/link";
 import AboutBody from "./AboutBody";
 import HomeBody from "./HomeBody";
 import Sidebar from "./Sidebar";
 
 export default function Navbar(body: JSX.Element | null): JSX.Element {
-    if(body == null) {
-        body = <div/>
+    if (body == null) {
+        body = <div />
     }
 
     return (
@@ -18,14 +19,16 @@ export default function Navbar(body: JSX.Element | null): JSX.Element {
             <Sidebar />
             <div className="h-10 w-full border border-gray-700 justify-center">
                 <h1 className="text-center text-2xl">
-                    <a href="/" className="rounded-md hover:bg-gray-900 font-bold">
-                        <span className="text-white hover:text-orange-600">
-                            Cyber
-                        </span>
-                        <span className="text-orange-600 hover:text-white">
-                            Orioles
-                        </span>
-                    </a>
+                    <Link href="/" className="rounded-md hover:bg-gray-900 font-bold">
+                        <div>
+                            <span className="text-white hover:text-orange-600 cursor-pointer">
+                                Cyber
+                            </span>
+                            <span className="text-orange-600 hover:text-white cursor-pointer">
+                                Orioles
+                            </span>
+                        </div>
+                    </Link>
                 </h1>
                 {body}
             </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,16 @@
 import { CameraIcon, ClipboardListIcon, HomeIcon, QuestionMarkCircleIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 export default function Sidebar() {
     return (
         <div className="w-10 border border-gray-700 bg-black h-screen">
-            <a href="/"><HomeIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5" /></a>
+            <Link href="/"><HomeIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5 cursor-pointer" /></Link>
             <div className="h-4"/>
-            <a href="/news"><ClipboardListIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5" /></a>
+            <Link href="/news"><ClipboardListIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5 cursor-pointer" /></Link>
             <div className="h-4"/>
-            <a href="/about"><QuestionMarkCircleIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5" /></a>
+            <Link href="/about"><QuestionMarkCircleIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5 cursor-pointer" /></Link>
             <div className="h-4"/>
-            <a href="/media"><CameraIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5"/></a>
+            <Link href="/media"><CameraIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-white rounded-full ml-0.5 cursor-pointer"/></Link>
         </div>
     )
 }
