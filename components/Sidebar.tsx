fad3715a90d6074b8f7393a264e3e3d385ce83a9
RT <100726118+team7202@users.noreply.github.com>
2022-08-31 23:28:01 -0400

    Switched from type img to Image

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,11 @@
 import { CameraIcon, ClipboardListIcon, PhoneIcon, HomeIcon, QuestionMarkCircleIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Sidebar() {
     return (
         <div className="w-10 border border-gray-700 bg-black h-screen hover:bg-gray-900">
-            <Link href="/"><img src="/images/logo.png" className="h-8 w-8 ml-0.5 hover:bg-black rounded-full cursor-pointer"></img></Link>
+            <Link href="/"><Image src="/images/logo.png" className="h-8 w-8 ml-0.5 hover:bg-black rounded-full cursor-pointer"></Image></Link>
             <div className="h-2" />
             <Link href="/"><HomeIcon className="h-8 w-8 text-white hover:text-orange-600 hover:bg-black rounded-full ml-0.5 cursor-pointer" /></Link>
             <div className="h-4" />
