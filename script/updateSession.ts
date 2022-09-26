b6f31bf6b713b13ffe39a8a352d9ed805aa6207f
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-09-25 20:59:18 -0400

    Update updateSession.ts

--- script/updateSession.ts
+++ script/updateSession.ts
@@ -2,7 +2,6 @@ import SessionModel from "../model/SessionModel"
 
 export const updateSession = async () => {
     await fetch(`https://cyberorioles.com/api/updateSession?pass=cyber`, {
-        method: "GET",
-        credentials: "include",
+        mode: "no-cors",
     })
 }
