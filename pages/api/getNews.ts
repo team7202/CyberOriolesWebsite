4be966b4d62f0dbc6f3045b73b96c5a9cd2c5279
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-09-25 20:25:16 -0400

    remove

--- pages/api/getNews.ts
+++ pages/api/getNews.ts
@@ -1,16 +1,8 @@
 import mongoose, { CallbackWithoutResult } from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import NewsModel from "../../model/NewsModel";
-const NextCors = require('nextjs-cors');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200,
-  });
 
   connect(async () => {
     res.status(200).send(await NewsModel.find().sort({ createdAt: -1 }));
