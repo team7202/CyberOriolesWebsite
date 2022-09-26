d4203fe9bc7e279464502f9ae35624e9ffa0d224
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-09-25 20:30:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://cyberorioles.com" },
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
