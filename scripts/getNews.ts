e9789cc2362e15998f770f0e6d8ddc16af42c447
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-09-28 12:05:05 -0400

    Update getNews.ts

--- scripts/getNews.ts
+++ scripts/getNews.ts
@@ -1,5 +1,4 @@
 export const getNews = async () => {
-    const res = await fetch(`http://localhost:3000/api/getNews`);
     const res = await fetch(`https://cyberorioles.com/api/getNews`);
     const data = await res.json();
     return data;
