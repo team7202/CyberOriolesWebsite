c86ed8b21e1e2ad6e28a65b2efc0fffb7d63bd75
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-10-02 20:06:39 -0400

    Made full screen image fixed

--- components/media/ImageFullscreen.tsx
+++ components/media/ImageFullscreen.tsx
@@ -15,7 +15,7 @@ export default function ImageFullscreen({ show, url, onClose }: { show: boolean,
     };
 
     const modalContent = show ? (
-        <div className="absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.85)] select-none">
+        <div className="fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.85)] select-none">
             <AiFillCloseCircle className="duration-1000 hover:duration-1000 ml-[90vw] mt-[2.5vh] absolute text-[5vh] text-white hover:text-orange-600 hover:cursor-pointer" onClick={(e) => handleCloseClick(e)}/>
             <div className="flex justify-center items-center h-full">
                 <img src={url} className="w-[40vw] h-[40vw]" />
