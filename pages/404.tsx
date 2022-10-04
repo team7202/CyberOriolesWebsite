3fb2d5e9a95cdbd113ed02dc796be1ddfd6afa6c
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-10-04 07:48:33 -0400

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,8 @@ import type { NextPage } from 'next'
 const FourOhFour: NextPage = (props: any) => {
     return (
       <div className="h-[94vh] flex items-center justify-center">
-            <span className="w-fit text-red-600 text-3xl">
-                <span className="pr-[0.5vw] border-r rounded-sm border-r-red-700">404</span><span className="ml-[0.5vw]">Page Not Found</span>
+            <span className="duration-1000 hover:duration-1000 w-fit text-red-600 hover:text-orange-600 text-3xl">
+                <span className="pr-[0.5vw] border-r rounded-sm border-r-red-700 hover:border-r-orange-700">404</span><span className="ml-[0.5vw]">Page Not Found</span>
             </span>
       </div>
     )
