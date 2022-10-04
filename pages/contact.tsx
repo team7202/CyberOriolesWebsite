53c92363339f002af0308ad6c29b472c279b8e3e
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-10-04 07:18:50 -0400

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 
 const Contact: NextPage = (props: any) => {
     return (
-      <div></div>
+      <div className="h-[94vh] flex items-center justify-center">
+        <span className="w-fit">Coming Soon</span> 
+      </div>
     )
 }
 
