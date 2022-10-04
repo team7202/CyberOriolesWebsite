9a3c422238e6a7ec796f9ede90fc29ce069c9083
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-10-04 07:16:55 -0400

    Update sponsors.tsx

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Sponsors: NextPage = (props: any) => {
     return (
-      <div className="flex items-center justify-center">
+      <div className="h-[95vh] flex items-center justify-center">
         <span className="w-fit">Coming Soon</span> 
       </div>
     )
