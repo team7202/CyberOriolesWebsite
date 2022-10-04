1eea110017392e8d90c0e0c0643b9debefaa628a
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-10-04 07:27:40 -0400

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const FourOhFour: NextPage = (props: any) => {
+    return (
+      <div className="h-[94vh] flex items-center justify-center">
+        <span className="w-fit text-red-600">404</span> 
+      </div>
+    )
+}
+
+export default FourOhFour;
