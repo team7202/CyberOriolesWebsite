1254b44e3fe1da68088dc843232d647e454e9dd8
Cyber Orioles <100726118+team7202@users.noreply.github.com>
2023-01-05 21:37:27 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navbar from '../components/nav/Navbar'
-import Head from 'next/head'
-import { NextComponentType, NextPageContext } from 'next'
-import Constants from '../scripts/Constants'
-import { useState } from 'react'
-import ImageFullscreen from '../components/media/ImageFullscreen'
+import Link from 'next/link'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react';
 
-interface Props { }
+export default function App({ Component, pageProps }: AppProps) {
 
-function MyApp({ Component, pageProps }: { Component: NextComponentType<NextPageContext, any, any>, pageProps: Props }) {
+  const [showButtons, setShowButtons] = useState(false);
 
   return (
-      <div className='h-screen absolute cursor-default bg-stone-900 font-[poppins] scrollbar-thin scrollbar-thumb-orange-400 hover:scrollbar-thumb-orange-500 active:scrollbar-thumb-orange-600 scrollbar-thumb-rounded-full scrollbar-track-black scrollbar-track-rounded-full' id='root'>
-        <Head>
-          <title>CyberOrioles</title>
-          <meta name="description" content="A Website for the 7202 Cyber Orioles Robotics Team" />
-        </Head>
-        <div className="h-screen w-screen">
-          <div className='w-[2vw] h-screen bg-neutral-800 rounded-r-full fixed' />
-          <div className='ml-[2vw] w-[96vw] h-screen bg-stone-900 absolute'>
-            <div className='h-[10vh]' />
-            <Component {...pageProps} />
+    <div className='w-screen h-screen bg-black text-orange-500 text-lg font-[Libre Franklin] overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-[rgba(1,1,1,0.8)] scrollbar-track-[rgba(1,1,1,0.8)]'>
+      <div id='navbar' className='w-screen sm:h-[3.5vh] lg:h-[5vh] flex justify-center items-center gap-[3vw] bg-gradient-to-br from-orange-600 via-amber-300 to-orange-600 rounded-b-full absolute'>
+        <Link href={"/"} className='absolute left-10 sm:text-[1.75vh] lg:text-[2.5vh] font-bold text-orange-600 hover:text-white duration-1000 bg-black sm:pl-[0.1vmin] sm:pt-[0.05vmin] sm:pr-[0.1vmin] sm:pb-[0.05vmin] md:pl-[calc(0.15vw+0.15vh)] md:pt-[calc(0.25vw+0.25vh)] md:pr-[calc(0.25vw+0.25vh)] md:pb-[calc(0.25vw+0.25vh)] rounded-full'>CyberOrioles</Link>
+        <Link href={"/"} className="sm:hidden md:block bg-[rgba(0,0,0,0.8)] hover:bg-[rgba(0,0,0,1)] pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-full hover:text-white duration-1000 font-bold">Home</Link>
+        <Link href={"/announcements"} className="sm:hidden md:block bg-[rgba(0,0,0,0.8)] hover:bg-[rgba(0,0,0,1)] pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-full hover:text-white duration-1000 font-bold">Announcements</Link>
+        <Link href={"/media"} className="sm:hidden md:block bg-[rgba(0,0,0,0.8)] hover:bg-[rgba(0,0,0,1)] pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-full hover:text-white duration-1000 font-bold">Media</Link>
+        <Link href={"/sponsors"} className="sm:hidden md:block bg-[rgba(0,0,0,0.8)] hover:bg-[rgba(0,0,0,1)] pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-full hover:text-white duration-1000 font-bold">Sponsors</Link>
+        {!showButtons ?
+          <GiHamburgerMenu className='md:hidden absolute right-10 text-2xl bg-[rgba(0,0,0,0.5)] pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-md duration-500' onClick={() => setShowButtons(true)} />
+          :
+          <GiHamburgerMenu className='md:hidden absolute right-10 text-2xl bg-black pl-[calc(0.25vw+0.25vh)] pt-[calc(0.25vw+0.25vh)] pr-[calc(0.25vw+0.25vh)] pb-[calc(0.25vw+0.25vh)] rounded-md rotate-[90deg] duration-500' onClick={() => setShowButtons(false)} />
+        }
+        {showButtons ?
+          <div className=''>
+            <div className='md:hidden absolute bg-gradient-to-br from-[rgba(234,88,12,0.9)] via-[rgba(252,211,77,0.9)] to-[rgba(234,88,12,0.9)] text-black w-[45vmin] h-fit pb-[1vmin] pt-[1vmin] right-5 top-[4vh] rounded-md duration-1000 flex flex-col items-center gap-3'>
+              <div className='h-[0.5vmin]' />
+              <Link href={"/"} className="bg-orange-400 pl-[1vmin] pt-[1vmin] pr-[1vmin] pb-[1vmin] shadow-[-5px_5px_0px_rgb(225,120,30)]">Home</Link>
+              <Link href={"/announcements"} className="bg-orange-400 pl-[1vmin] pt-[1vmin] pr-[1vmin] pb-[1vmin] shadow-[-5px_5px_0px_rgb(225,120,30)]">Announcements</Link>
+              <Link href={"/media"} className="bg-orange-400 pl-[1vmin] pt-[1vmin] pr-[1vmin] pb-[1vmin] shadow-[-5px_5px_0px_rgb(225,120,30)]">Media</Link>
+              <Link href={"/sponsors"} className="bg-orange-400 pl-[1vmin] pt-[1vmin] pr-[1vmin] pb-[1vmin] shadow-[-5px_5px_0px_rgb(225,120,30)]">Sponsors</Link>
+              <div className='h-[0.5vmin]' />
+            </div>
           </div>
-          <div className='ml-[98vw] w-[2vw] h-screen bg-neutral-800 rounded-l-full fixed' />
-          <Navbar />
-        </div>
+          :
+          <div className='md:hidden absolute bg-[rgba(0,0,0,0)] w-[45vmin] h-[10vmin] right-5 top-[4vh] rounded-md duration-1000 flex flex-col'>
+
+          </div>
+        }
       </div>
+      {/* <div className='sm:h-[3.5vh] lg:h-[5vh]' /> */}
+      <Component {...pageProps} />
+    </div>
   )
 }
-
-export default MyApp
