e8b0a541a4dd88c1c5f39b53daf35cc97ebf6454
Cyber Orioles <100726118+team7202@users.noreply.github.com>
2023-01-05 21:37:56 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 
-export default function Home(props: any) {
-    return (
-        <>
-            <Head>
-                <title>CyberOrioles | Announcements</title>
-                <meta name="description" content="Official Announcements from the 7202 CyberOrioles" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className='w-screen h-screen flex flex-col justify-center items-center'>
-                <div className='mt-[5vh] sm:w-[90vmin] sm:text-[16px] lg:text-[24px] lg:w-[90vw] sm:h-[50vmin] h-[85vh] bg-[rgba(16,16,16,0.5)] rounded-lg overflow-y-auto hover:bg-[rgba(16,16,16,0.9)] duration-1000'>
-                    <iframe src="https://player.twitch.tv/?channel=firstupdatesnow&parent=www.cyberorioles.com&parent=cyberorioles.com&muted=false" className='border-2 border-neutral-900 rounded-md sm:w-[70vmin] lg:w-[70vw] sm:h-[50vmin] h-[85vh]' frameBorder="0" allowFullScreen={true} scrolling="no" height="1920" width="1080"></iframe>
-                </div>
-            </main>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CyberOrioles | Home</title>
+        <meta name="description" content="Official of site of the 7202 CyberOrioles" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='w-screen h-screen flex flex-col justify-center items-center'>
+        <div className='sm:w-[75vmin] lg:w-[50vw] h-[25vh] bg-[rgba(16,16,16,0.5)] rounded-lg flex flex-col justify-center items-center hover:bg-[rgba(16,16,16,0.9)] duration-1000'>
+          <h1 className='text-[2.5vh] text-center'>Welcome to the offical website of the CyberOrioles Robotics team!</h1>
+          <p className='mt-[2vh] text-[2vh]'>Let us introduce ourselves!</p>
+          <p className='text-[2vh] text-center'>We are the CyberOrioles, a robotics team based in Morrice Michigan!</p>
+        </div>
+      </main>
+    </>
+  )
 }
