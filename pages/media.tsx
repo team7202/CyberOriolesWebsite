fa51bab4a37f695111b34fd015c1143cea22e45c
Cyber Orioles <100726118+team7202@users.noreply.github.com>
2023-01-07 11:54:44 -0500

    Update media.tsx

--- pages/media.tsx
+++ pages/media.tsx
@@ -11,7 +11,7 @@ export default function Home(props: any) {
             </Head>
             <main className='w-screen h-screen flex flex-col justify-center items-center'>
                 <div className='mt-[5vh] sm:w-[90vmin] sm:text-[16px] lg:text-[24px] lg:w-[90vw] sm:h-[50vmin] h-[85vh] bg-[rgba(16,16,16,0.5)] rounded-lg overflow-y-auto hover:bg-[rgba(16,16,16,0.9)] duration-1000'>
-                    <iframe src="https://player.twitch.tv/?channel=firstupdatesnow&parent=www.cyberorioles.com&parent=cyberorioles.com&muted=false" className='border-2 border-neutral-900 rounded-md sm:w-[70vmin] lg:w-[70vw] sm:h-[50vmin] h-[85vh]' frameBorder="0" allowFullScreen={true} scrolling="no" height="1920" width="1080"></iframe>
+                    <iframe src="https://player.twitch.tv/?channel=firstinspires&parent=www.cyberorioles.com&parent=cyberorioles.com&muted=false" className='border-2 border-neutral-900 rounded-md sm:w-[70vmin] lg:w-[70vw] sm:h-[50vmin] h-[85vh]' frameBorder="0" allowFullScreen={true} scrolling="no" height="1920" width="1080"></iframe>
                 </div>
             </main>
         </>
